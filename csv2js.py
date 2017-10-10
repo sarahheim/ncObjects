734d131160e72443f5005d2df3bb53e4abc2270b
Sarah Heim <scheim@ucsd.edu>
2017-10-10 08:53:44 -0700

    1) added flagStats functions that count the number of flags for each variable 2) added csv2js.py which makes a html with Highcharts of the output csv flag counts 3) minor edit to editOldNC - edited drop all '_flag' variables 4) editOldNCs - added regex for filename

---
+++ csv2js.py
@@ -0,0 +1,52 @@
+import os, time
+import pandas as pd
+
+def allStations(stations, postName):
+    dict = {}
+    for s in stations:
+        fname  = s+postName
+        dict[s] = pd.read_csv(fname, index_col=0)
+
+    allPn = pd.Panel(dict)
+    print allPn.shape
+    divs = ''
+    jsCharts = ''
+    for i, v in enumerate(allPn.major_axis):
+        divs += '<div id="container{}" style="height: 250px; min-width: 310px"></div>\n'.format(i)
+        print v#, allPn.major_xs(v)
+        seriesArr = []
+        for s in allPn.major_xs(v):
+            seriesArr.append({'name': s, 'data':allPn[s, v, :].fillna('null').tolist()})
+        # print allPn.minor_axis[0]
+        jsCharts += '''
+$('#container{}').highcharts({{
+    title : {{ text : "{}" }},
+    yAxis: {{ title: {{ text: "Flags" }} }},
+    plotOptions: {{ series: {{ pointStart: {} }} }},
+    series : {}
+}}); '''.format(i, v, allPn.minor_axis[0], seriesArr)
+    # print allPn[s, v, :].tolist()
+
+    html = open(postName.split('.')[0]+'.html', 'w')
+    html.write('''<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+<script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
+<script src="http://code.highcharts.com/stock/highstock.js"></script>
+<script src="http://code.highcharts.com/stock/modules/exporting.js"></script>
+
+<script>
+$(function () {{
+{}
+}});
+</script>
+</head>
+<body>
+<h1>{}<h1>
+{}
+</body>
+</html>'''.format(jsCharts, postName, divs))
+    html.close()
+
+allStations(['ucsb', 'uci', 'ucla', 'ucsd'], '_v1.csv')
